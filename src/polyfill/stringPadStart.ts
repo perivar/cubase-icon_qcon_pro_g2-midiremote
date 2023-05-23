08d616e6a560553a408d7d88da2b1e33f64a9755
Per Ivar Nerseth <perivar@nerseth.com>
2023-05-23 23:41:25 +0200

    made sure we get typescript es5 warnngs

--- src/polyfill/stringPadStart.ts
+++ src/polyfill/stringPadStart.ts
@@ -1,30 +1,30 @@
-// declare global {
-//   interface String {
-//     padStart(targetLength: number, padString: string): string;
-//   }
-// }
+declare global {
+  interface String {
+    padStart(targetLength: number, padString: string): string;
+  }
+}
 
-// // PIN String.prototype.padStart polyfill
-// if (!String.prototype.padStart) {
-//   String.prototype.padStart = function (targetLength: number, padString: string) {
-//     targetLength = Math.floor(targetLength) || 0;
-//     if (targetLength < this.length) return String(this);
+// PIN String.prototype.padStart polyfill
+if (!String.prototype.padStart) {
+  String.prototype.padStart = function (targetLength: number, padString: string) {
+    targetLength = Math.floor(targetLength) || 0;
+    if (targetLength < this.length) return String(this);
 
-//     padString = padString ? String(padString) : " ";
+    padString = padString ? String(padString) : " ";
 
-//     let pad = "";
-//     const len = targetLength - this.length;
-//     let i = 0;
-//     while (pad.length < len) {
-//       if (!padString[i]) {
-//         i = 0;
-//       }
-//       pad += padString[i];
-//       i++;
-//     }
+    let pad = "";
+    const len = targetLength - this.length;
+    let i = 0;
+    while (pad.length < len) {
+      if (!padString[i]) {
+        i = 0;
+      }
+      pad += padString[i];
+      i++;
+    }
 
-//     return pad + String(this).slice(0);
-//   };
-// }
+    return pad + String(this).slice(0);
+  };
+}
 
-// export {};
+export {};
